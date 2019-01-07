58207e4f99bdd9a2e216b1363834f6574f4322a5
rimmertsijtsma <r.d.sijtsma@student.rug.nl>
2019-01-07 10:25:59 +0100

    Reformatted the register form

--- rooms/views/register.php
+++ rooms/views/register.php
@@ -37,60 +37,85 @@
                     <form action="<?= $form_action ?>" method="POST">
 
                     <div class="pd-15">&nbsp;</div>
-                        <div class="form-group">
-                            <label for="username">Username</label>
+                        <div class="form-group row">
+                            <label for="username" class="col-sm-2 col-form-label">Username</label>
+                            <div class="col-sm-3">
                             <input type="text" class="form-control" id="inputUsername" placeholder="J.deboer" name="username" required>
+                            </div>
+
+                            <label for="password" class="col-sm-2 col-form-label">Password</label>
+                            <div class="col-sm-3">
+                                <input type="password" class="form-control" id="password" placeholder="*********" name="password" required>
+                            </div>
                         </div>
-                        <div class="form-group">
-                            <label for="firstname">First name</label>
+                        <div class="form-group row">
+                            <label for="firstname" class="col-sm-2 col-form-label">First name</label>
+                            <div class="col-sm-3">
                             <input type="text" class="form-control" id="first_name" placeholder="Johan" name="first_name" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="lastname">Last name</label>
+                            </div>
+
+                            <label for="lastname" class="col-sm-2 col-form-label">Last name</label>
+                            <div class="col-sm-3">
                             <input type="text" class="form-control" id="last_name" placeholder="de Boer" name="last_name" required>
+                            </div>
                         </div>
-                        <div class="form-group">
-                            <label for="birthdate">Date of birth</label>
+                        <div class="form-group row">
+                            <label for="birthdate" class="col-sm-2 col-form-label">Date of birth</label>
+                            <div class="col-sm-3">
                             <input type="date" class="form-control" id="birthdate" placeholder="1990-01-01" name="birthdate" required>
+                            </div>
+
+                            <label for="inputType" class="col-sm-2 col-form-label">Gender</label>
+                            <div class="col-sm-3">
+                                <select class="custom-select" name="sex">
+                                    <option value="male">Male</option>
+                                    <option value="female">Female</option>
+                                    <option value="other">Other</option>
+                                </select>
+                            </div>
                         </div>
-                        <div class="form-group">
-                            <label for="sex">Gender</label>
-                            <input type="radio" class="form-control" id="sex" value="male" name="sex">Male<br>
-                            <input type="radio" class="form-control" id="sex" value="female" name="sex">Female<br>
-                            <input type="radio" class="form-control" id="sex" value="other" name="sex">Other
-                        </div>
-                        <div class="form-group">
-                            <label for="email">E-mail</label>
+                        <div class="form-group row">
+                            <label for="email" class="col-sm-2 col-form-label">E-mail</label>
+                            <div class="col-sm-3">
                             <input type="email" class="form-control" id="email" placeholder="j.deboer@deboer.nl" name="email" required>
+                            </div>
+
+                            <label for="inputType" class="col-sm-2 col-form-label">Role</label>
+                            <div class="col-sm-3">
+                                <select class="custom-select" name="role">
+                                    <option value="owner">Owner</option>
+                                    <option value="tenant">Tenant</option>
+                                </select>
+                            </div>
                         </div>
-                        <div class="form-group">
-                            <label for="password">Password</label>
-                            <input type="password" class="form-control" id="password" placeholder="*********" name="password" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="role">Role</label>
-                            <input type="radio" class="form-control" id="role" value="owner" name="role">Owner<br>
-                            <input type="radio" class="form-control" id="role" value="tenant" name="role">Tenant
-                        </div>
-                        <div class="form-group">
-                            <label for="phonenumber">Phonenumber</label>
+                        <div class="form-group row">
+                            <label for="phonenumber" class="col-sm-2 col-form-label">Phonenumber</label>
+                            <div class="col-sm-3">
                             <input type="tel" class="form-control" id="phonenumber" placeholder="0610012002" name="phonenumber" pattern="[0]{1}[6]{1}[0-9]{8}" required>
+                            </div>
                         </div>
-                        <div class="form-group">
-                            <label for="studies">Enter your study</label>
+                        <div class="form-group row">
+                            <label for="studies" class="col-sm-2 col-form-label">Enter your study</label>
+                            <div class="col-sm-3">
                             <input type="text" class="form-control" id="studies" placeholder="Informatiekunde" name="studies">
-                        </div>
-                        <div class="form-group">
-                            <label for="profession">Enter your profession</label>
+                            </div>
+
+                            <label for="profession" class="col-sm-2 col-form-label">Enter your profession</label>
+                            <div class="col-sm-3">
                             <input type="text" class="form-control" id="profession" placeholder="Student-assistant" name="profession">
+                            </div>
                         </div>
-                        <div class="form-group">
-                            <label for="biography">Tell us something about you</label>
-                            <input type="text" class="form-control" id="biography" placeholder="Hi, my name is..." name="biography">
+                        <div class="form-group row">
+                            <label for="biography" class="col-sm-2 col-form-label">Tell us something about you</label>
+                            <div class="col-sm-3">
+                            <textarea type="text" class="form-control" id="biography" placeholder="Hi, my name is..." name="biography"></textarea>
+                            </div>
                         </div>
-                        <div class="form-group">
-                            <label for="picture">Upload a profile picture</label>
-                            <input type="file" class="form-control" id="picture" name="picture">
+                        <div class="form-group row">
+                            <label for="picture" class="col-sm-2 col-form-label">Upload a profile picture</label>
+                            <div class="col-sm-4">
+                                <input type="file" class="form-control" id="picture" name="picture">
+                            </div>
                         </div>
                         <button type="submit" name="Submit" class="btn btn-info">Register</button>
                     </form>
